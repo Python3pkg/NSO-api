b28be0a0f0f155329e1b0f646b41996bb3699676
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-08-07 23:39:54 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
         name = 'pennnso',
         packages = ['pennnso'],
-        version = '0.1',
+        version = '0.2',
         description = 'Code base to scrape an ical that describes events, times, locations and more regarding NSO events.',
         author = 'Adam Domingoes',
         author_email = 'pjladd22@gmail.com',
         url = 'https://github.com/pennlabs/NSO-api',
-        download_url = 'https://github.com/pennlabs/NSO-api/tarball/0.1',
+        download_url = 'https://github.com/pennlabs/NSO-api/tarball/0.2',
         keywords = ['penn','nso','new','student','orientation','university','Pennsylvania','labs'],
         classifiers = [],
 )

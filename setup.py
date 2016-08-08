747646c90cec55ea3e90254a8e10ccd161c78d12
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-08-07 21:29:54 -0400

    Configuration issues

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-        name = 'nso-penn',
-        packages = ['nso-penn'],
-        version = '0.2',
+        name = 'nsopenn',
+        packages = ['nsopenn'],
+        version = '0.1',
         description = 'Code base to scrape an ical that describes events, times, locations and more regarding NSO events.',
         author = 'Adam Domingoes',
         author_email = 'pjladd22@gmail.com',
         url = 'https://github.com/pennlabs/NSO-api',
-        download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+        download_url = 'https://github.com/pennlabs/NSO-api/tarball/0.1',
         keywords = ['penn','nso','new','student','orientation','university','Pennsylvania','labs'],
         classifiers = [],
 )
